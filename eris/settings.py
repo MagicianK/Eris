30b9cc3db6508ddfbedb4a6c775519f59b425093
MagicianK <justperson1938@gmail.com>
2022-04-15 18:02:18 +0600

    little changes

--- eris/settings.py
+++ eris/settings.py
@@ -128,7 +128,7 @@ STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
-MEDIA_ROOT = BASE_DIR/"game/resources"
+MEDIA_ROOT = BASE_DIR / "game/resources"
 MEDIA_URL = '/resources/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
