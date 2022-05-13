3faab140a69e7c5f6b15d64fc50236df4ef865cf
MagicianK <justperson1938@gmail.com>
2022-05-13 18:08:34 +0600

    dsf

--- eris/settings.py
+++ eris/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 import os
 from pathlib import Path
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "admin.settings")
+django.setup()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
