1d42ba2210763e616db10ec8ecfddf3594c390c7
MagicianK <justperson1938@gmail.com>
2022-05-13 17:55:31 +0600

    some changes for deploy

--- eris/settings.py
+++ eris/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-fxv3fy6-pv4tz3@(=y#lj8ifxivfyo*!3lu%%wc4#fsscqk%hr'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+#DEBUG = True
 
 ALLOWED_HOSTS = ['erisgameword.herokuapp.com', '127.0.0.1']
 AUTH_USER_MODEL = 'game.CustomUser'
@@ -157,7 +157,7 @@ LOGOUT_REDIRECT_URL = "/login_page/"
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT  = os.path.join(BASE_DIR, 'staticfiles')
+#STATIC_ROOT  = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
