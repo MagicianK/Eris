a56a5756707e81d0d6ce97ce67778949a0ea4388
MagicianK <justperson1938@gmail.com>
2022-05-26 12:13:07 +0600

    hellod

--- eris/settings.py
+++ eris/settings.py
@@ -169,6 +169,7 @@ LOGOUT_REDIRECT_URL = "/login_page/"
 
 STATIC_URL = '/static/'
 STATIC_ROOT  = os.path.join(BASE_DIR, 'static')
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
