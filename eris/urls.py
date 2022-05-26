d6ba308f648cd51d0de49456f651d0cb720057e6
MagicianK <justperson1938@gmail.com>
2022-05-26 12:20:52 +0600

    hellod

--- eris/urls.py
+++ eris/urls.py
@@ -17,7 +17,6 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -25,4 +24,3 @@ urlpatterns = [
     path('accounts/', include('allauth.urls')),
     path('', include('django.contrib.auth.urls')), # login url, logout url
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-urlpatterns += staticfiles_urlpatterns()
